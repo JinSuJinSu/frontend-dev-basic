5bfa55105f68d374e210456f8bd85b8864049f60
JinsuJinsu <hjs928@naver.com>
2022-02-22 14:17:16 +0900

    finish js basic

--- javascript-practices/basic/ex14.js
+++ javascript-practices/basic/ex14.js
@@ -1,9 +1,29 @@
 console.log("=============global object=================");
 
-global.n1 = '둘리';
-console.log(n1, global.n1);
+global.name = '둘리';
+console.log(name, global.name);
 
 
 //cf
-let n2 = '둘리';
-console.log(n2, global.n2);
+let ename = '둘리';
+console.log(ename, global.ename);
+
+console.log("========function의 객체 메소드 apply ================");
+var f1 = function(){
+    console.log(this);
+}
+f1();
+
+// this 채우기
+var o = {
+    name:"마이클"
+};
+f1.apply(o);
+
+console.log("========function의 객체 메소드 call ================");
+var f2 = function(s1, s2){
+    console.log(s1 +" " +s2 + " " + this.name);
+};
+f2('hi','');
+f2.call(o, 'hi','again');
+f2.apply(o, ['hi','again']);
---
+++ javascript-practices/basic/ex17.js
@@ -0,0 +1,33 @@
+/*
+
+Data 객체 메소드()
+*/
+
+// 현재 시간
+var now = new Date();
+console.log(now);
+
+// 2022년 02-22
+var d1 = new Date(2022,1,22)
+console.log(d1);
+
+// 2022년 02-22 22:22:22
+var d2 = new Date(2022,1,22,22,22,22)
+console.log(d2);
+
+// 객체 메소드
+console.log(
+    "년도:" + now.getFullYear() + "\n",
+    "월:" + (now.getMonth()+1) + "\n",
+    "일:" + now.getDate() + "\n",
+    "시:" + now.getHours() + "\n",
+    "분:" + now.getMinutes() + "\n",
+    "초:" + now.getSeconds() + "\n",
+    "밀리초:" + now.getMilliseconds() + "\n"
+)
+
+d2.setFullYear(2024);
+console.log(d2);
+
+d2.setMonth(10);
+console.log(d2);
