d8d8365c3e27e37defcd2605dd4a1428a7a1a947
JinsuJinsu <hjs928@naver.com>
2022-02-23 20:16:29 +0900

    add ch07 files

--- ch05/src/main/webapp/tabbox.js
+++ ch05/src/main/webapp/tabbox.js
@@ -1,25 +1,24 @@
 let tabBox = {
 	
-	_onWindowLoad:function(){
+	_onWindowLoad: function() {
 		let divTabBox = document.getElementsByClassName("tab-box")[0];
 		let ul = divTabBox.childNodes[1];
-		
+	
 		let liTabs = ul.getElementsByTagName("li");
-		for(let i=0; i<liTabs.length; i++){
-			liTabs[i].addEventListener('click',this.onTabClicked);
+		for(let i = 0; i < liTabs.length; i++) {
+			liTabs[i].addEventListener("click", this._onTabClicked);
 		}
 	},
 	
-	_onTabClicked:function(){
-		 // unselected
-		 let lisSelected = document.getElementsByClassName("selected");
-		 (lisSelected.length===1) && (lisSelected[0].className= "");
-		 
-		 //select
-		 this.className = "selected";
-	},
+	_onTabClicked: function() {
+		//unselected
+		var lisSelected = document.getElementsByClassName("selected");
+		(lisSelected.length === 1) && (lisSelected[0].className = "");
 	
+		// select
+		this.className='selected';
+	},
 	init: function() {
-		window.addEventListener('load',this._onWindowLoad.bind(this));
-	}
+		window.addEventListener('load', this._onWindowLoad.bind(this));
+	}	
 }
